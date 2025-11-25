f4eebb3d1555901b7d9971131a465b93198bdc98
Nmcp1 <141276162+Nmcp1@users.noreply.github.com>
2025-11-25 14:52:57 -0300

    Fix CSRF on Render

--- noteboard/settings.py
+++ noteboard/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -80,10 +81,12 @@ WSGI_APPLICATION = 'noteboard.wsgi.application'
 # ---------------------------------------------------------
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Fallback a SQLite si no hay DATABASE_URL (por ejemplo en tu PC)
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 
